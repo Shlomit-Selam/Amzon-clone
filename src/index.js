92d6a56679c7565aa1f32db378bd9e5e76d4bc57
shlomit-selam <shlomitdaniel@gmail.com>
2021-01-28 23:05:49 -0500

    h

--- src/index.js
+++ src/index.js
@@ -19,3 +19,15 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAGPVQ-6EFkDSeH7wxkW21jaRhyI_W-6jg",
+  authDomain: "clone-e1aa7.firebaseapp.com",
+  projectId: "clone-e1aa7",
+  storageBucket: "clone-e1aa7.appspot.com",
+  messagingSenderId: "278770807309",
+  appId: "1:278770807309:web:5a4eba073601c3de275b44",
+  measurementId: "G-NHK69Y44BH"
+};
